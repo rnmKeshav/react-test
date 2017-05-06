9f1eb8a0b94f9a9c1ba0c358318ff00a32cd086d
Keshav kumar <keshav12raju@gmail.com>
2017-05-06 16:04:42 +0530

    adding dropdown

--- src/clientApp.jsx
+++ src/clientApp.jsx
@@ -1,20 +1,44 @@
 var React = require('react')
 var ReactDOM = require('react-dom')
-var MyTitle = require('./components/myTitle')
+var Dropdown = require('./components/dropdown')
+
 
 class App extends React.Component {
   constructor(props) {
     super(props);
   }
 
+  getOptions() {
+    let options = [
+      {
+        label: 'Option 1',
+        value: 1
+      }, 
+      {
+        label: 'Option 2',
+        value: 2
+      }
+    ];
+
+    return options;
+  }
+
+  onOptionSelect(selectedOption) {
+    
+  }
+
   render () {
+    let options = this.getOptions();
+
     return (
       <div className="app-container">
         <div className="app-background"></div>
         <div className="searchbox-wrapper u-pos-m-m">
           <div className="search-github-text">Search Github user</div>
-          <input type="text" className="search-github-input" placeholder="Please enter user name"/>
-          <button type="button" className="search-button">Search</button>
+          <Dropdown options={options} onOptionSelect={this.onOptionSelect.bind(this)}/>
+          <div className="u-t-center">
+            <button type="button" className="search-button">Search</button>
+          </div>
         </div>
       </div>
     )
